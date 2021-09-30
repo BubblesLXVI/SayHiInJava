a44074d30e12c663647f84fb5b03efc23449b55f
Alin <alin.66.campos@gmail.com>
2021-09-30 17:01:33 +0000

    added hello world 5 times

--- SayHi.java
+++ SayHi.java
@@ -1,5 +1,9 @@
-public class SayHi{
+public class HelloWorld{
 	public static void main(String[] args){
-		System.out.println("Say Hi");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
 	}
 }
